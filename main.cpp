8b9797163f268179f55e434c0891740bc3794f05
eric hsieh <eric_hsieh@alphanetworks.com>
2019-08-01 14:11:45 +0800

    gst_pipeline_new

--- main.cpp
+++ main.cpp
@@ -2,20 +2,29 @@
 
 int main(int argc, char *argv[])
 {
-    GstElement *element;
-    gchar *name;
+    GstElement *pipeline;
+    GstElement *source, *filter, *sink;
 
     // init gstreamer
     gst_init(&argc, &argv);
 
-    // create element
-    element = gst_element_factory_make("fakesrc", "source");
+    // create pipeline
+    pipeline = gst_pipeline_new("my-pipeline");
 
-    // get the name
-    g_object_get(G_OBJECT(element), "name", &name, NULL);
-    g_print("The name of the element is %s\n", name);
-    g_free(name);
+    // create elements
+    source = gst_element_factory_make("fakesrc", "source");
+    filter = gst_element_factory_make("identity", "filter");
+    sink = gst_element_factory_make("fakesink", "sink");
 
-    gst_object_unref((GST_OBJECT(element)));
+    // add elements to pipeline before link
+    gst_bin_add_many(GST_BIN(pipeline), source, filter, sink, NULL);
+
+    // link
+    if (!gst_element_link_many(source, filter, sink, NULL)) {
+        g_warning("Failed to link elements");
+    }
+
+    // only need to unref pipeline, children will release, too.
+    gst_object_unref (GST_OBJECT(pipeline));
     return 0;
 }
