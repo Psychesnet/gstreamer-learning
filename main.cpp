58248bb6a36de810f43ca48950830dfab7760440
eric hsieh <psychesnet@gmail.com>
2019-09-04 13:43:12 +0800

    pad-added

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <gst/gst.h>
 
+static void cb_new_pad(GstElement *element, GstPad *pad, gpointer data)
+{
+    gchar *name = gst_pad_get_name(pad);
+    g_print("A new pad %s was created\n", name);
+    g_free(name);
+}
+
 int main(int argc, char *argv[])
 {
-    GstElement *pipeline, *bin;
-    GstElement *source, *filter, *sink;
+    GstElement *pipeline, *source, *demux, *sink;
+    GMainLoop *loop;
 
     // init gstreamer
     gst_init(&argc, &argv);
@@ -11,26 +18,27 @@ int main(int argc, char *argv[])
     // create pipeline
     pipeline = gst_pipeline_new("my-pipeline");
 
-    // create bin
-    bin = gst_bin_new("mi-bin");
-
     // create elements
-    source = gst_element_factory_make("fakesrc", "source");
-    filter = gst_element_factory_make("identity", "filter");
+    source = gst_element_factory_make("filesrc", "source");
+    g_object_set(source, "location", argv[1], NULL);
+    demux = gst_element_factory_make("oggdemux", "demuxer");
     sink = gst_element_factory_make("fakesink", "sink");
 
-    // add elements to bin
-    gst_bin_add_many(GST_BIN(bin), source, filter, sink, NULL);
+    // put together a pipeline
+    gst_bin_add_many(GST_BIN(pipeline), source, demux, sink, NULL);
+    gst_element_link_pads(source, "src", demux, "sink");
 
-    // add bin to pipeline
-    gst_bin_add(GST_BIN(pipeline), bin);
+    // listen for newly created pads
+    g_signal_connect(demux, "pad-added", G_CALLBACK(cb_new_pad), NULL);
 
-    // link
-    if (!gst_element_link_many(source, filter, sink, NULL)) {
-        g_warning("Failed to link elements");
-    }
+    // start the pipeline
+    gst_element_set_state(GST_ELEMENT(pipeline), GST_STATE_PLAYING);
+
+    loop = g_main_loop_new(NULL, FALSE);
 
     // only need to unref pipeline, children will release, too.
+    gst_element_set_state(GST_ELEMENT(pipeline), GST_STATE_NULL);
     gst_object_unref (GST_OBJECT(pipeline));
+    g_main_loop_unref(loop);
     return 0;
 }
